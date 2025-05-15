130c1c0ec2acab58428207f781d009354e0f2c14
Midori Kochiya <i@reimu.moe>
2025-05-15 17:00:35 +0800

    Bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "moe.reimu.ancsreceiver"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     // https://developer.android.com/build/dependencies#dependency-info-play
